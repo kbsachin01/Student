b6265ae0c4c6747a3200b9318164914f1ee6b42b
kbsachin01 <kbsachin01@gmail.com>
2016-01-16 10:27:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('*', function (req, res) {
   res.sendfile('./public/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 })
